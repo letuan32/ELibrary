c456a9ed4a8929f78e48345853ee82a0b538591a
HoangMinhTrong <tronggaw2@gmail.com>
2021-07-19 00:04:00 -0700

    Add-CategoryJs

--- ELibrary_Team 1/wwwroot/js/category.js
+++ ELibrary_Team 1/wwwroot/js/category.js
@@ -17,16 +17,42 @@ function loadDataTable() {
                 "render": function (data) {
                     return `
                             <div class="text-center">
-    <a href="/Authenticated/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
-        <i class="fas fa-edit"></i>
-    </a>
-    <a onclick=Delete("/Authenticated/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
-        <i class="fas fa-trash-alt"></i>
-    </a>
-</div>
+                                <a href="/Authenticated/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i> 
+                                </a>
+                                <a onclick=Delete("/Authenticated/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i> 
+                                </a>
+                            </div>
                            `;
                 }, "width": "40%"
             }
         ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
