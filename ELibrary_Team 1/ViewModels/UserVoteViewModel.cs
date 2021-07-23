fd643edcff6eb7e744ae9b7e66c3c1f0d6502d83
letuan32 <letuanlttt@gmail.com>
2021-07-23 11:01:29 +0700

    Change Controller Area, Ajax Url
    Change Controller Area, Ajax Url

---
+++ ELibrary_Team 1/ViewModels/UserVoteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ELibrary_Team_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELibrary_Team_1.ViewModels
+{
+    public class UserVoteViewModel
+    {
+        UserVote UserVote { get; set; }
+    }
+}
