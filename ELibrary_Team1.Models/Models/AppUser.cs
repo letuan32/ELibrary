04c11ad1d368430eb9eecc46d25cb1a992b23a74
HoangMinhTrong <tronggaw2@gmail.com>
2021-07-25 15:27:34 +0700

    Role-NotMapped

--- ELibrary_Team1.Models/Models/AppUser.cs
+++ ELibrary_Team1.Models/Models/AppUser.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ELibrary_Team_1.Models
 {
     public class AppUser:IdentityUser
     {
+        [NotMapped]
+        public string Role;
+
         //
 
         public string FullName { get; set; }
