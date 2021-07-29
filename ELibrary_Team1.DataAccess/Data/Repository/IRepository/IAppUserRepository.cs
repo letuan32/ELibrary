f966c1088e7eb170d0af8b2bc258a57334e574c0
HoangMinhTrong <tronggaw2@gmail.com>
2021-07-29 14:11:02 +0700

    Repository-AppUser-Update

--- ELibrary_Team1.DataAccess/Data/Repository/IRepository/IAppUserRepository.cs
+++ ELibrary_Team1.DataAccess/Data/Repository/IRepository/IAppUserRepository.cs
@@ -1,7 +1,9 @@
 ﻿using ELibrary_Team_1.Models;
+using System.Threading.Tasks;
+
 namespace ELibrary_Team1.DataAccess.Data.Repository.IRepository
 {
-    public interface IAppUserRepository:IGenericRepository<AppUser>
+    public interface IAppUserRepository : IGenericRepository<AppUser>
     {
     }
 }
