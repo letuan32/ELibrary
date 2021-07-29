2d403fe6f4ac937aff9c03e58e3619264926ee27
HoangMinhTrong <tronggaw2@gmail.com>
2021-07-29 14:13:30 +0700

    Role-Controller-Razor-Page

---
+++ ELibrary_Team 1/Areas/Admin/Pages/Role/Add.cshtml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Elibrary_Team1.Areas.Admin.Pages.Role
+{
+    public class AddModel : PageModel
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AddModel(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [TempData] // Sử dụng Session
+        public string StatusMessage { get; set; }
+
+        public class InputModel
+        {
+            public string ID { set; get; }
+
+            [Required(ErrorMessage = "Must enter role")]
+            [Display(Name = "Name of Role")]
+            [StringLength(100, ErrorMessage = "{0} long {2} to {1} Characters.", MinimumLength = 3)]
+            public string Name { set; get; }
+
+        }
+        [BindProperty]
+        public InputModel Input { set; get; }
+
+        [BindProperty]
+        public bool IsUpdate { set; get; }
+
+        public IActionResult OnGet() => NotFound("NotFound");
+        public IActionResult OnPost() => NotFound("NotFound");
+        public IActionResult OnPostStartNewRole()
+        {
+            StatusMessage = "Enter information to create new role";
+            IsUpdate = false;
+            ModelState.Clear();
+            return Page();
+        }
+        public async Task<IActionResult> OnPostStartUpdate()
+        {
+            StatusMessage = null;
+            IsUpdate = true;
+            if (Input.ID == null)
+            {
+                StatusMessage = "Error: No information about Role";
+                return Page();
+            }
+            var result = await _roleManager.FindByIdAsync(Input.ID);
+            if (result != null)
+            {
+                Input.Name = result.Name;
+                ViewData["Title"] = "Update role : " + Input.Name;
+                ModelState.Clear();
+            }
+            else
+            {
+                StatusMessage = "Error:No information about Role ID = " + Input.ID;
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddOrUpdate()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                StatusMessage = null;
+                return Page();
+            }
+
+            if (IsUpdate)
+            {
+                // CẬP NHẬT
+                if (Input.ID == null)
+                {
+                    ModelState.Clear();
+                    StatusMessage = "Error: No information about Role";
+                    return Page();
+                }
+                var result = await _roleManager.FindByIdAsync(Input.ID);
+                if (result != null)
+                {
+                    result.Name = Input.Name;
+                    var roleUpdateRs = await _roleManager.UpdateAsync(result);
+                    if (roleUpdateRs.Succeeded)
+                    {
+                        StatusMessage = "Update role Success";
+                        return RedirectToPage("./Index");
+                    }
+                    else
+                    {
+                        StatusMessage = "Error: ";
+                        foreach (var er in roleUpdateRs.Errors)
+                        {
+                            StatusMessage += er.Description;
+                        }
+                    }
+                }
+                else
+                {
+                    StatusMessage = "Error: Not found Role update";
+                }
+
+            }
+            else
+            {
+                // TẠO MỚI
+                var newRole = new IdentityRole(Input.Name);
+                var rsNewRole = await _roleManager.CreateAsync(newRole);
+                if (rsNewRole.Succeeded)
+                {
+                    StatusMessage = $"Create role Success: {newRole.Name}";
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                    StatusMessage = "Error: ";
+                    foreach (var er in rsNewRole.Errors)
+                    {
+                        StatusMessage += er.Description;
+                    }
+                }
+            }
+
+            return Page();
+
+        }
+    }
+}
---
+++ ELibrary_Team 1/Areas/Admin/Pages/Role/Delete.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Elibrary_Team1.Areas.Admin.Pages.Role
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DeleteModel(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public class InputModel
+        {
+            [Required]
+            public string ID { set; get; }
+            public string Name { set; get; }
+
+        }
+
+        [BindProperty]
+        public InputModel Input { set; get; }
+
+        [BindProperty]
+        public bool isConfirmed { set; get; }
+
+        [TempData] // Sử dụng Session
+        public string StatusMessage { get; set; }
+
+        public IActionResult OnGet() => NotFound("NotFound");
+
+        public async Task<IActionResult> OnPost()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return NotFound("Can't Delete");
+            }
+
+            var role = await _roleManager.FindByIdAsync(Input.ID);
+            if (role == null)
+            {
+                return NotFound("Not Found role to Delete");
+            }
+
+            ModelState.Clear();
+
+            if (isConfirmed)
+            {
+                //Xóa
+                await _roleManager.DeleteAsync(role);
+                StatusMessage = "Deleted " + role.Name;
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                Input.Name = role.Name;
+                isConfirmed = true;
+
+            }
+
+            return Page();
+        }
+    }
+}
