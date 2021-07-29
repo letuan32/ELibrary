fadb1f4f0cebe9c154a7e955ebcd276b46fcaf8a
HoangMinhTrong <tronggaw2@gmail.com>
2021-07-29 14:12:43 +0700

    New-ViewModel

---
+++ ELibrary_Team 1/ViewModels/ManageUserRolesViewModelcs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELibrary_Team_1.ViewModels
+{
+    public class ManageUserRolesViewModelcs
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ ELibrary_Team 1/ViewModels/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ELibrary_Team_1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELibrary_Team_1.ViewModels
+{
+    public class UserViewModel
+    {
+        public AppUser AppUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
